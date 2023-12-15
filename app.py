eccd19dc8f4027b51c5dfd7cfba6427dfd1afda3
PRATHMESHMANE <62151445+PRATHMESHMANE@users.noreply.github.com>
2023-12-16 00:59:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,92 @@
+import cv2 as cv
+import numpy as np
+import streamlit as st
+from streamlit_option_menu import option_menu
+from mediapipe.python.solutions.drawing_utils import _normalized_to_pixel_coordinates
+from utils.utils import get_models, get_image_file,  mediapipe_detection, opencv_detection
+from utils.utils import mp_face_detection, mp_drawing, emotion_dict, rescale, enhance_image, enhance_image_with_adaptive_histogram_equalization, enhance_image_with_histogram_equalization, enhance_image_with_unsharp_masking, enhance_image_with_bilateral_filter
+import tensorflow as tf
+from tensorflow.keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.models import Model
+
+# Add Sidebar and Main Window style
+st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child{
+        width: 330px
+    }
+    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child{
+        width: 330px
+        margin-left: -350px
+    }
+    [data-testid="stSidebar"] [data-testid="stMarkdownContainer"] > div:first-child h1{
+        padding: 0rem 0rem 0rem 0rem;
+        text-align: center;
+        font-size: 2rem;
+    }
+    .css-1544g2n.e1fqkh3o4 {
+        padding-top: 4rem;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+# Basic App Scaffolding
+st.title('Scene Detection')
+st.divider()
+
+with st.sidebar:
+    st.title('Scenery')
+    st.divider()
+    # Define available pages in selection box
+    app_mode = option_menu("", [ "Image"],
+                           icons=["images"], menu_icon="list", default_index=0,
+                           styles={
+                               "icon": {"font-size": "1rem"},
+                               "nav-link": {"font-family": "roboto", "font-size": "1rem", "text-align": "left"},
+                               "nav-link-selected": {"background-color": "tomato"},
+                           }
+                           )
+
+
+# Image Page
+if app_mode == 'Image':
+
+    # Sidebar
+
+    model = get_models()
+    detection_type = st.sidebar.radio('',['Scene Detection', 'Indiviual'])
+    st.sidebar.divider()
+
+    detection_confidence = 0.5
+
+
+    image = get_image_file()
+
+
+
+    # Enhance the inputed image
+    # image = enhance_image(image=image)
+    # image = enhance_image_with_adaptive_histogram_equalization(image)
+   
+    st.sidebar.write('Image Enhancement Options')
+    enhancement_option = st.sidebar.selectbox('Select Enhancement Technique', ['None', 'Histogram Equalization', 'Adaptive Histogram Equalization', 'Unsharp Masking', 'Bilateral Filter'])
+    if enhancement_option == 'Histogram Equalization':
+        enhanced_image = enhance_image_with_histogram_equalization(image)
+    elif enhancement_option == 'Adaptive Histogram Equalization':
+        enhanced_image = enhance_image_with_adaptive_histogram_equalization(image)
+    elif enhancement_option == 'Unsharp Masking':
+        enhanced_image = enhance_image_with_unsharp_masking(image)
+    elif enhancement_option == 'Bilateral Filter':
+        enhanced_image = enhance_image_with_bilateral_filter(image)
+    else:
+        enhanced_image = image
+
+
+    if detection_type == 'Scene Detection':
+        mediapipe_detection(detection_confidence, enhanced_image, model, 'With full image')
+    else:
+        opencv_detection(enhanced_image, model, 'With full image')
+    
---
+++ utils.py
@@ -0,0 +1,172 @@
+import os
+import tempfile
+
+import cv2 as cv
+import numpy as np
+import mediapipe as mp
+import streamlit as st
+import tensorflow as tf
+
+from mediapipe.python.solutions.drawing_utils import _normalized_to_pixel_coordinates
+from .modelutils import my_new_model
+
+from tensorflow.keras.utils import get_file
+
+
+# emotions dictionary
+emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fear", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+
+# load the cascade file
+face_cascade = cv.CascadeClassifier('./haarcascade_frontalface_alt.xml')
+
+model_url = 'https://fer-model.s3.ap-south-1.amazonaws.com/base_1_overfit.h5'
+
+
+# load mediapipe model and drawing utils
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
+model_type = None
+
+
+# get model on the choice of the user
+def get_models():
+    global model_type
+    model_type ='Model with VGG'
+    # load emotion model
+    if model_type == 'Model with VGG':
+        model_path = get_file('base_1_overfit.h5', model_url, cache_subdir='models')
+        model = tf.keras.models.load_model(model_path)
+        print(model.summary())
+        return model
+    
+
+
+def rescale():
+    return model_type == 'Model with VGG'
+
+
+# Image enhancement options
+def enhance_image(image):
+    # Apply sharpening kernel
+    kernel = np.array([[-1, -1, -1],
+                       [-1, 9, -1],
+                       [-1, -1, -1]])
+    enhanced_image = cv.filter2D(image, -1, kernel)
+
+    return enhanced_image
+
+def enhance_image_with_histogram_equalization(image):
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    enhanced_image = cv.equalizeHist(gray)
+    return cv.cvtColor(enhanced_image, cv.COLOR_GRAY2BGR)
+
+def enhance_image_with_adaptive_histogram_equalization(image):
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    enhanced_image = clahe.apply(gray)
+    return cv.cvtColor(enhanced_image, cv.COLOR_GRAY2BGR)
+
+def enhance_image_with_unsharp_masking(image):
+    blurred = cv.GaussianBlur(image, (0, 0), 10)
+    enhanced_image = cv.addWeighted(image, 1.5, blurred, -0.5, 0)
+    return enhanced_image
+
+def enhance_image_with_bilateral_filter(image):
+    enhanced_image = cv.bilateralFilter(image, d=9, sigmaColor=75, sigmaSpace=75)
+    return enhanced_image
+
+def get_image_file():
+    img_file_buffer = st.file_uploader("Upload an Image", type=["jpg", "jpeg", "png"])
+    st.divider()
+
+    # Check if image is uploaded or not
+    if img_file_buffer is not None:
+        # read uploaded image
+        image_file = np.asarray(bytearray(img_file_buffer.read()), dtype=np.uint8)
+        return cv.imdecode(image_file, 1)
+    else:
+        # read demo image
+        demo_image = "./assets/group1.png"
+        return cv.imread(demo_image)
+
+
+def mediapipe_detection(detection_confidence, image, model, mode):
+    with mp_face_detection.FaceDetection(model_selection=1,
+                                         min_detection_confidence=detection_confidence) as face_detection:
+        results = face_detection.process(cv.cvtColor(image, cv.COLOR_BGR2RGB))
+
+    image_rows, image_cols, _ = image.shape
+    out_img = image.copy()
+    emotion_predictions = []
+
+    if results.detections:
+        for detection in results.detections:
+            try:
+                # Draw face detection box
+                mp_drawing.draw_detection(out_img, detection)
+
+                box = detection.location_data.relative_bounding_box
+                x = _normalized_to_pixel_coordinates(box.xmin, box.ymin, image_cols, image_rows)
+                y = _normalized_to_pixel_coordinates(box.xmin + box.width, box.ymin + box.height, image_cols,
+                                                     image_rows)
+
+                # Crop image to face
+                cimg = image[x[1] - 20:y[1] + 20, x[0] - 20:y[0] + 20]
+                if rescale():
+                    cropped_img = np.expand_dims(cv.resize(cimg, (48, 48)), 0)
+                else:
+                    cropped_img = np.expand_dims(cv.resize(cimg, (48, 48)), 0) / 255.
+
+                # get model prediction
+                pred = model.predict(cropped_img)
+                idx = int(np.argmax(pred))
+
+                cv.putText(out_img, emotion_dict[idx], (x[0], x[1] - 20), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2,
+                           cv.LINE_AA)
+
+                emotion_predictions.append(pred)
+
+            except Exception:
+                pass
+
+        if mode == 'With full image':
+            st.image(out_img, channels="BGR", use_column_width=True)
+
+        if emotion_predictions:
+            avg_prediction = np.mean(emotion_predictions, axis=0)
+            avg_idx = int(np.argmax(avg_prediction))
+
+            st.write('Scene type : ', emotion_dict[avg_idx])
+
+
+
+def opencv_detection(image, model, mode):
+    out_img = image.copy()
+
+    # Detect the faces
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4, minSize=(60, 60))
+
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        cv.rectangle(out_img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        # Crop image to face
+        cimg = image[y:y + h, x:x + w]
+        if rescale():
+            cropped_img = np.expand_dims(cv.resize(cimg, (48, 48)), 0)
+        else:
+            cropped_img = np.expand_dims(cv.resize(cimg, (48, 48)), 0) / 255.
+
+        # get model prediction
+        pred = model.predict(cropped_img)
+        idx = int(np.argmax(pred))
+
+        cv.putText(out_img, emotion_dict[idx], (x, y), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2, cv.LINE_AA)
+
+        if mode == 'With full image':
+            st.write('Emotion: ', emotion_dict[idx])
+            st.image(cv.resize(cimg, (300, 300)), channels="BGR", caption='Cropped Image')
+
+
+
